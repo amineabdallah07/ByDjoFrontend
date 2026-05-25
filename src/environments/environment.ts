da9a62df2a7cfead20ebd92b106b961578ae016f
Amine0702 <amineabdallah2k22@gmail.com>
2026-05-25 16:06:30 +0200

    commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  apiUrl: "https://bydjobackend-production.up.railway.app/api",
+  storeName: "BY DJO",
+  currency: "TND",
+  currencySymbol: "د.ت",
+  whatsapp: "+216XXXXXXXX",
+  facebook: "bydjo",
+  instagram: "bydjo",
+};
